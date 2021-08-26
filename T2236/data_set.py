c47c91689162bb93c0747d186a12ffee57785610
T2236 <eric9687@naver.com>
2021-08-26 06:41:28 -0900

    8/26

--- T2236/data_set.py
+++ T2236/data_set.py
@@ -24,3 +24,42 @@ class TrainDataset(Dataset):
 
     def __len__(self):
         return len(self.img_paths)
+
+class TrainDataset_Gender(Dataset):
+    def __init__(self, data_df,transform):
+        self.data_df = data_df
+        self.img_paths = list(data_df["path"])
+        self.label = list(data_df["gender_code"])
+        self.transform = transform
+            
+    def __getitem__(self, index):
+        image = Image.open(self.img_paths[index])
+
+        if self.transform:
+            image = self.transform(image)
+        
+                
+        return image, self.label[index]
+
+    def __len__(self):
+        return len(self.img_paths)
+
+
+class TrainDataset_Age(Dataset):
+    def __init__(self, data_df,transform):
+        self.data_df = data_df
+        self.img_paths = list(data_df["path"])
+        self.label = list(data_df["age_code"])
+        self.transform = transform
+            
+    def __getitem__(self, index):
+        image = Image.open(self.img_paths[index])
+
+        if self.transform:
+            image = self.transform(image)
+        
+                
+        return image, self.label[index]
+
+    def __len__(self):
+        return len(self.img_paths)
--- T2236/train_gender.py
+++ T2236/train_gender.py
@@ -16,12 +16,12 @@ import torch.optim as optim
 
 from torchvision import transforms
 from torchvision import models
-from torchvision.transforms import Resize, ToTensor, Normalize,RandomHorizontalFlip
+from torchvision.transforms import Resize, ToTensor, Normalize
 import matplotlib.pyplot as plt
 
-from data_set import TrainDataset
+from data_set import TrainDataset_Gender
 
-NUM_EPOCH = 30
+NUM_EPOCH = 10
 BATCH_SIZE = 32
 LEARNING_RATE =  0.002
 phase = "train"
@@ -44,16 +44,15 @@ data_df = pd.read_csv("/opt/ml/train_data_path_and_class.csv")
 transform = transforms.Compose([
     Resize((512, 384), Image.BILINEAR),
     ToTensor(),
-    RandomHorizontalFlip(0.5),
     Normalize(mean=(0.5, 0.5, 0.5), std=(0.2, 0.2, 0.2)),
 ])
-dataset = TrainDataset(data_df,transform)
+dataset = TrainDataset_Gender(data_df,transform)
 
 # DataLoader
 loader = DataLoader(
     dataset,
     batch_size = BATCH_SIZE,
-    shuffle= True
+    shuffle=False
 )
 
 
@@ -115,72 +114,72 @@ for epoch in range(NUM_EPOCH):
 print("Train Finished!")
 
 
-# print("Model's state_dict:")
-# for param_tensor in model.state_dict():
-#     print(param_tensor, "\t", model.state_dict()[param_tensor].size())
+# # print("Model's state_dict:")
+# # for param_tensor in model.state_dict():
+# #     print(param_tensor, "\t", model.state_dict()[param_tensor].size())
 
 # # ### 옵티마이저의 state_dict 출력
-# print("Optimizer's state_dict:")
-# for var_name in optimizer.state_dict():
-#     print(var_name, "\t", optimizer.state_dict()[var_name])
+# # print("Optimizer's state_dict:")
+# # for var_name in optimizer.state_dict():
+# #     print(var_name, "\t", optimizer.state_dict()[var_name])
 
-PATH = model_arch +"model_saved.pt"
-torch.save({'epoch': NUM_EPOCH,
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            'loss': loss,
-            }, PATH)
+# # PATH = model_arch +"model_saved.pt"
+# # torch.save({'epoch': NUM_EPOCH,
+# #             'model_state_dict': model.state_dict(),
+# #             'optimizer_state_dict': optimizer.state_dict(),
+# #             'loss': loss,
+# #             }, PATH)
 
 
-class TestDataset(Dataset):
-    def __init__(self, img_paths, transform):
-        self.img_paths = img_paths
-        self.transform = transform
+# class TestDataset(Dataset):
+#     def __init__(self, img_paths, transform):
+#         self.img_paths = img_paths
+#         self.transform = transform
 
-    def __getitem__(self, index):
-        image = Image.open(self.img_paths[index])
+#     def __getitem__(self, index):
+#         image = Image.open(self.img_paths[index])
 
-        if self.transform:
-            image = self.transform(image)
-        return image
+#         if self.transform:
+#             image = self.transform(image)
+#         return image
 
-    def __len__(self):
-        return len(self.img_paths)
+#     def __len__(self):
+#         return len(self.img_paths)
 
-test_dir = '/opt/ml/input/data/eval'
-submission = pd.read_csv(os.path.join(test_dir, 'info.csv'))
-image_dir = os.path.join(test_dir, 'images')
+# test_dir = '/opt/ml/input/data/eval'
+# submission = pd.read_csv(os.path.join(test_dir, 'info.csv'))
+# image_dir = os.path.join(test_dir, 'images')
 
-# Test Dataset 클래스 객체를 생성하고 DataLoader를 만듭니다.
-image_paths = [os.path.join(image_dir, img_id) for img_id in submission.ImageID]
-transform = transforms.Compose([
-    Resize((512, 384), Image.BILINEAR),
-    ToTensor(),
-    Normalize(mean=(0.5, 0.5, 0.5), std=(0.2, 0.2, 0.2)),
-])
-dataset = TestDataset(image_paths, transform)
+# # Test Dataset 클래스 객체를 생성하고 DataLoader를 만듭니다.
+# image_paths = [os.path.join(image_dir, img_id) for img_id in submission.ImageID]
+# transform = transforms.Compose([
+#     Resize((512, 384), Image.BILINEAR),
+#     ToTensor(),
+#     Normalize(mean=(0.5, 0.5, 0.5), std=(0.2, 0.2, 0.2)),
+# ])
+# dataset = TestDataset(image_paths, transform)
 
-loader = DataLoader(
-    dataset,
-    shuffle=False
-)
+# loader = DataLoader(
+#     dataset,
+#     shuffle=False
+# )
 
-# 모델을 정의합니다. (학습한 모델이 있다면 torch.load로 모델을 불러주세요!)
-model.eval()
+# # 모델을 정의합니다. (학습한 모델이 있다면 torch.load로 모델을 불러주세요!)
+# model.eval()
 
-# 모델이 테스트 데이터셋을 예측하고 결과를 저장합니다.
-all_predictions = []
-for images in loader:
-    with torch.no_grad():
-        images = images.to(device)
-        pred = model(images)
-        pred = pred.argmax(dim=-1)
-        all_predictions.extend(pred.cpu().numpy())
-submission['ans'] = all_predictions
+# # 모델이 테스트 데이터셋을 예측하고 결과를 저장합니다.
+# all_predictions = []
+# for images in loader:
+#     with torch.no_grad():
+#         images = images.to(device)
+#         pred = model(images)
+#         pred = pred.argmax(dim=-1)
+#         all_predictions.extend(pred.cpu().numpy())
+# submission['ans'] = all_predictions
 
-# 제출할 파일을 저장합니다.
-submission.to_csv(os.path.join(test_dir, 'submission_eff.csv'), index=False)
-print('test inference is done!')
+# # 제출할 파일을 저장합니다.
+# submission.to_csv(os.path.join(test_dir, 'submission_eff.csv'), index=False)
+# print('test inference is done!')
 
 
 
