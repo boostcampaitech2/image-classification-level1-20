f38f27971c4f35067dce4d49d62949b2ab411022
ssail09 <ssail@naver.com>
2021-09-03 11:28:48 +0000

    model

--- mask-classification/model/model.py
+++ mask-classification/model/model.py
@@ -5,8 +5,6 @@ import timm
 
 
 class EfficientNet(nn.Module):
-
-
     def __init__(self, num_classes):
         super(EfficientNet, self).__init__()
         self.model = timm.create_model('tf_efficientnet_b4', num_classes=num_classes, pretrained=True)
@@ -16,15 +14,28 @@ class EfficientNet(nn.Module):
         return output
 
 ## resnet은 수정
-class resnet34(nn.Module):
+class resnet50(nn.Module):
     def __init__(self, num_classes):
-        super(resnet34).__init__()
-        self.ft_model=torchvision.models.resnet34(pretrained=True)
+        super(resnet50).__init__()
+        self.ft_model=torchvision.models.resnet50(pretrained=True)
 
 
         num_ftrs = self.ft_model.fc.in_features
-        self.ft_model.fc=nn.Linear(num_ftrs,num_classes)  
+        self.ft_model.fc=nn.Linear(num_ftrs,num_classes) 
 
     def forward(self, x):
         output = self.ft_model(x)
         return output
+
+class VIT(nn.Module):
+
+
+    def __init__(self, num_classes):
+        super(VIT, self).__init__()
+        self.model = timm.create_model('vit_tiny_patch16_224', num_classes=num_classes, pretrained=True)
+    
+    def forward(self, x):
+        output = self.model(x)
+        return output
+
+
