2c58e4d250494206144f8c5c58be1e3c72a2c158
PARK JIN HAN <jinhan814@naver.com>
2021-09-04 07:28:11 +0000

    modified ensemble.py

--- mask-classification/ensemble.py
+++ mask-classification/ensemble.py
@@ -4,7 +4,7 @@ import pandas as pd
 def compress(csv_mask, csv_gender, csv_age):
     ret = csv_mask
     for i in range(1, 12601):
-        ret[i][1] = csv_mask[i][1] * 6 + csv_gender[i][1] * 3 + csv_age[i][1]
+        ret[i][1] = int(csv_mask[i][1]) * 6 + int(csv_gender[i][1]) * 3 + int(csv_age[i][1])
     return ret
 
 def conv(n):
@@ -17,7 +17,7 @@ def vote(p: list, data: list) -> int:
     mask = [0.] * 3
     gender = [0.] * 2
     age = [0.] * 3
-    for i in range(5):
+    for i in range(3):
         _mask, _gender, _age = conv(data[i])
         mask[_mask] += p[0][i]
         gender[_gender] += p[1][i]
